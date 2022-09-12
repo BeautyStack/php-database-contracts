254875401e43d854590b0531a743a1ad660dda4e
Ken Lalobo <lalobo@gmail.com>
2022-09-12 17:04:00 +0100

    add new exception

--- src/RepositoryInterface.php
+++ src/RepositoryInterface.php
@@ -3,13 +3,14 @@
 namespace Beautystack\Database\Contracts;
 
 use Beautystack\Database\Contracts\Exception\DuplicateEntityException;
+use Beautystack\Database\Contracts\Exception\EntityClassMismatchException;
 use Beautystack\Database\Contracts\Exception\EntityNotFoundException;
 use Beautystack\Value\Contracts\Identity\Id;
 
 interface RepositoryInterface
 {
     /**
-     * @throws EntityNotFoundException
+     * @throws EntityNotFoundException|EntityClassMismatchException
      */
     public function findById(Id $id): ModelInterface;
 
